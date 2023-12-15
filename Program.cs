26fbfd9e303e39f8cf4f493b9b6f0a8928c3633b
Aline Anjos <aline.aanjosx@gmail.com>
2023-12-15 17:55:41 -0300

    Desafio de projeto concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Iphone _iphone = new Iphone("9999999","IPhone 15","111111111-11",128);
+_iphone.InstalarAplicativo("Facebook");
+
+Nokia _nokia = new Nokia("9999999","Nokia x","222222222-22",32);
+_iphone.InstalarAplicativo("Instagram");
+
+
